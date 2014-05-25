5cd346397ecdd1a725e90a421a460f4524cc6f30
Vajk Szecsi <szvajk@users.noreply.github.com>
2014-05-25 23:51:05 +0200

    Created solution for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a list that holds the already computed matrix - inverse matrix pairs.
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix as an input and returns a list which contains the cached data if it already exists.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL ## sets cache empty in this environment
+  set <- function(y) {  
+    x <<- y
+    m <<- NULL
+  }  ## Function for putting the cache value into x
+  get <- function() x ##Returns precalculated value
+  setinverse <- function(solve) m <<- solve ##computes the inverse and puts it in m, the cache
+  getinverse <- function() m ## returns newly computed value
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) ## output holding data defined above in a list
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input the output of makeCacheMatrix. It calculates the inverse matrix and stores it, so in case it needs to be recomputed it can be recalled from memory.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse() ##sets m as the getinverse value from the list (4th), that is filled if data is precalculated.
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get() ##if m is empty then retieve the original input matrix
+  m <- solve(data, ...) ## m holds the inverse matrix
+  x$setinverse(m) #store the result in x
+  m ## output answer
 }
